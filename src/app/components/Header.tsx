715ea5467f679b1713c044db3be1733ccebb102b
SandeepYadav99 <sandeepyadav7one@gmail.com>
2023-06-18 12:36:34 +0530

    [fix] Header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,7 @@
-import CheckOut from "../pages/CheckOut/CheckOut";
 import { RootState } from "../store/store";
 import Sidebar from "./SideBar";
 import { useSelector } from 'react-redux'
+
 const Header = (): JSX.Element => {
     const subtotal = useSelector((state: RootState) => state.cart.subtotal);
     const cartItems = useSelector((state: RootState) => state.cart.items);
@@ -11,9 +11,9 @@ const Header = (): JSX.Element => {
         <>
             <header className="flex justify-between p-4 w-[100%] fixed top-0 z-10 bg-gray-600 text-white">
                 <h1 className="black text-xl">Logo Ispm</h1>
-                <div className="flex w-[20%] justify-between">
-                    <p><span>({totalQuantity})</span> item in Cart - </p>
-                    <span>Total Amount ${subtotal}</span>
+                <div className="flex w-[22%] justify-around">
+                    <p><span>({totalQuantity})</span> item in Cart-<span>Total Amount ${subtotal}</span></p>
+                   
                 </div>
             </header>
             <aside className="w-64 top-0 bg-gray-800 text-white fixed h-screen m">
